99cf8a25ea635e3378967f439e1d1779b6cb362f
vinityadav <vinityadav1321@gmial.com>
2023-07-27 13:24:29 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="vinityadav",
+    author_email="vinityadav1321@gmial.com",
+    packages=find_packages(),
+    install_requires=[],
+)
